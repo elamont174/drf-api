138c838f3421467e6676c47083e9efcd232500e4
Emma Lamont <ejwl_92@hotmail.com>
2024-10-16 18:15:50 +0000

    Fix typos

--- profiles/models.py
+++ profiles/models.py
@@ -2,22 +2,26 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
-class Profiles(models.Model):
+
+class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeFielsd(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', default='../default_profile_qrxywt')
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_qdjgyp'
+    )
 
-    class Meta: 
+    class Meta:
         ordering = ['-created_at']
 
-    def__str__(self):
+    def __str__(self):
         return f"{self.owner}'s profile"
+
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.cfreate(owner=instance)
+        Profile.objects.create(owner=instance)
 
 post_save.connect(create_profile, sender=User)
